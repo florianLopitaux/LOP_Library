ccf2f3a940863132f10ceb64ef508b1d7f88f30d
florianLopitaux <florian.lopitaux@gmail.com>
2026-06-10 13:26:42 +0200

    Added resources package

---
+++ library-app/resources/resources.component.lisp
@@ -0,0 +1,22 @@
+(bis:def-component ::de.h-da.lop.library-app.resources
+  (:nicknames :resources)
+  (:documentation "Package that relate to all types of resources in the library")
+  (:languages :functional :oo :bis :rule :workflow :test)    ;;; delete languages not used here
+  (:import  
+      ;;; no dependencies
+	  
+   )
+  (:export
+    ;; DATATYPES
+    eBookType
+
+    ;; CLASSES
+    BookReference
+    get-title
+    get-author
+    get-type
+
+    ;; FUNCTIONS
+    findAllResearchBookReferences
+   )
+  )

bee9476fc06a06150ae7639f240a40227ff90061
florianLopitaux <florian.lopitaux@gmail.com>
2026-07-07 19:50:04 +0200

    Cleaned code of borrowing-system package

--- library-app/borrowing-system/borrowing-system.component.lisp
+++ library-app/borrowing-system/borrowing-system.component.lisp
@@ -1,38 +1,42 @@
 (bis:def-component ::de.h-da.lop.library-app.borrowing-system
   (:nicknames :borrowing-system)
   (:documentation "Package that manage stuff about book borrowing (borrow, return, ...)")
-  (:languages :functional :oo :bis :rule :workflow :test)    ;;; delete languages not used here
+  (:languages :functional :oo :bis :rule :workflow :test)
   (:import
+    ;; PACKAGE DEPENDENCIES
     :users
     :resources
     :stock
 
    )
   (:export
+    ;; DATATYPES
+    eCustomerRating
+    eReturnCase
     tDate
-    make-tDate
+    make-tDate ;; constructor of struct tDate
 
-    ;; CLASSES
+    ;; CLASSES / ENTITIES
     BorrowingRecord
-    get-start-date
-    get-due-date
-    get-is-returned
-    set-is-returned
-    get-customer
-    get-book
+    get-start-date ;; getter of class BorrowingRecord
+    get-due-date ;; getter of class BorrowingRecord
+    get-is-returned ;; getter of class BorrowingRecord
+    get-customer ;; getter of class BorrowingRecord
+    get-book ;; getter of class BorrowingRecord
 
-    ;; FUNCTIONS
-    dateToStringFormat
-    toHistoryStringFormat
-    getCustomerRating
-    isBookItemAvailable
-    borrowBookItem
-    returnBookItem
-    calculDueDate
+    ;; ENTITY MANAGER FUNCTIONS
     findAllRecordsFromBookItem
     findAllRecordsFromCustomer
     findAllNormalRecordsFromCustomer
     findAllLateRecordsFromCustomer
     findAllDamageRecordsFromCustomer
+
+    ;; SERVICE FUNCTIONS
+    dateToStringFormat
+    getCustomerRating
+    isBookItemAvailable
+    borrowBookItem
+    returnBookItem
+    calculDueDate
    )
   )
